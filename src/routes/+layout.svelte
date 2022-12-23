8d70df91b9ca6e9a31985b81e693321e141b582f
Yuen Ler Chow <lerchow@gmail.com>
2022-12-23 14:39:50 -0500

    config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
+<script lang="ts">
   import '../app.css'
+  import { Container } from 'sveltestrap/src'
+  import { onMount } from 'svelte'
+  // Import the functions you need from the SDKs you need
+  import firebase, { initializeApp } from 'firebase/app'
+  import { getAnalytics } from 'firebase/analytics'
+  import 'firebase/auth'
+
+  onMount(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBuFDxD2Zt2i40-dSNZz3Xrt0YypVRj6OA',
+      authDomain: 'hackharvard-portal.firebaseapp.com',
+      projectId: 'hackharvard-portal',
+      storageBucket: 'hackharvard-portal.appspot.com',
+      messagingSenderId: '831224207920',
+      appId: '1:831224207920:web:ae366f9b14464726f5abf5',
+      measurementId: 'G-N72ZTKXS9Y'
+    }
+    const app = initializeApp(firebaseConfig)
+    const analytics = getAnalytics(app)
+  })
 </script>
 
-<slot />
+<Container>
+  <slot />
+</Container>
---
+++ src/routes/signup.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Row, Col } from 'sveltestrap/src'
+</script>
+
+<Row>
+  <Col>
+    <h1>Sign up</h1>
+  </Col>
+</Row>
