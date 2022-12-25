5b7cca3541c6a23592620ee57e36cb313523158d
dseum22 <dseum22@gmail.com>
2022-12-26 00:04:14 +0400

    Select definitions

---
+++ src/lib/data/index.js
@@ -0,0 +1,15 @@
+import ethnicitiesJson from './ethnicities.json'
+import gendersJson from './genders.json'
+import schoolsJson from './schools.json'
+import worldJson from './world.json'
+import shirtSizeJson from './shirtSizes.json'
+import dietaryRestrictionsJson from './dietaryRestrictions.json'
+
+export {
+  ethnicitiesJson,
+  gendersJson,
+  schoolsJson,
+  worldJson,
+  shirtSizeJson,
+  dietaryRestrictionsJson
+}
