85057544fd593d56d212b8f4aaf2232bf5cf6b6e
dseum22 <dseum22@gmail.com>
2022-12-31 16:28:56 +0400

    Firebase update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,30 +1,20 @@
 import { dev } from '$app/environment'
 import { derived, readable } from 'svelte/store'
-import {
-  PUBLIC_FIREBASE_USE_SERVER,
-  PUBLIC_FIREBASE_API_KEY,
-  PUBLIC_FIREBASE_AUTH_DOMAIN,
-  PUBLIC_FIREBASE_PROJECT_ID,
-  PUBLIC_FIREBASE_STORAGE_BUCKET,
-  PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-  PUBLIC_FIREBASE_APP_ID,
-  PUBLIC_FIREBASE_MEASUREMENT_ID
-} from '$env/static/public'
 import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 let config
 if (dev) {
-  config = PUBLIC_FIREBASE_USE_SERVER
+  config = import.meta.env?.VITE_FIREBASE_API_KEY
     ? {
-        apiKey: PUBLIC_FIREBASE_API_KEY,
-        authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-        appId: PUBLIC_FIREBASE_APP_ID,
-        measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
       }
     : { apiKey: 'demo', authDomain: 'demo.firebaseapp.com' }
 } else {
