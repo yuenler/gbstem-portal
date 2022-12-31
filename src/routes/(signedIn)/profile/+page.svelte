d3f5f2fe0f0243efdc45342d468387bbd024cd0c
dseum22 <dseum22@gmail.com>
2022-12-31 11:34:45 +0400

    Changed layout

--- src/routes/(signedIn)/profile/+page.svelte
+++ src/routes/(signedIn)/profile/+page.svelte
@@ -9,7 +9,7 @@
 
 <div class="grid md:grid-cols-2">
   <h1 class="font-bold text-5xl md:text-6xl mb-8">Profile</h1>
-  <div class="grid gap-6">
+  <div class="flex flex-col gap-6 items-center">
     {#if !emailVerified}
       <div
         class="w-full p-3 rounded-md shadow flex items-center gap-2 bg-red-200 mt-2"
