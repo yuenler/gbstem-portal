197da395b399a1cdf0507cb2ee67a943452aa085
dseum22 <dseum22@gmail.com>
2023-01-01 17:19:38 +0400

    Dashboard additions

--- src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
+++ src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
@@ -1,1 +1,39 @@
-<div>dashboard</div>
+<script>
+  import Card from '$lib/components/Card.svelte'
+  import { getDoc, doc } from 'firebase/firestore'
+  import { fade } from 'svelte/transition'
+  import { user, db } from '$lib/firebase'
+
+  const asyncData = new Promise((resolve, reject) => {
+    let data = {
+      application: '',
+      group: {}
+    }
+    user.loaded().then(() => {
+      getDoc(doc($db, 'applications', $user.uid)).then(res => {
+        if (res.exists()) {
+          const application = res.data()
+          if (application.approved) {
+            data.application = 'Approved! We look forward to seeing you.'
+          } else {
+            data.application = application.submitted ? 'Submitted and in review!' : 'In progress.'
+          }
+        } else {
+          data.application = 'Not started.'
+        }
+        resolve(data)
+      })
+    })
+  })
+</script>
+
+{#await asyncData then data}
+  <div class="grid grid-cols-2" transition:fade|local={{ duration: 150 }}>
+    <Card>
+      <svelte:fragment slot="title">Application</svelte:fragment>
+      <div class="text-xl">{data.application}</div>
+    </Card>
+  </div>
+{:catch}
+  <div>Error loading data. Please try again.</div>
+{/await}
