ebc611f3e5274bbcdc4fd2facbc073349a770b7b
Yuen Ler Chow <lerchow@gmail.com>
2023-01-03 15:07:33 -0500

    remove dev

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,21 +6,32 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 let config
-if (dev) {
-  config = import.meta.env?.VITE_FIREBASE_API_KEY
-    ? {
-        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-        appId: import.meta.env.VITE_FIREBASE_APP_ID,
-        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-      }
-    : { apiKey: 'demo', authDomain: 'demo.firebaseapp.com' }
-} else {
-  // figure out
-}
+config = import.meta.env?.VITE_FIREBASE_API_KEY
+  ? {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  }
+  : { apiKey: 'demo', authDomain: 'demo.firebaseapp.com' }
+// if (dev) {
+//   config = import.meta.env?.VITE_FIREBASE_API_KEY
+//     ? {
+//         apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//         authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//         projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//         storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//         messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+//         appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//         measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+//       }
+//     : { apiKey: 'demo', authDomain: 'demo.firebaseapp.com' }
+// } else {
+//   // figure out
+// }
 
 export const app = readable(initializeApp(config))
 
