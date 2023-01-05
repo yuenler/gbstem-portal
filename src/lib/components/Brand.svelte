a473090574ab56d70e166add426b2ae213806b1e
Yuen Ler Chow <lerchow@gmail.com>
2023-01-05 12:55:01 -0500

    hackharvard favicon

--- src/lib/components/Brand.svelte
+++ src/lib/components/Brand.svelte
@@ -1,7 +1,7 @@
 <div>
   <div class="inline-flex items-center gap-2 rounded-md border border-gray-200 p-2 shadow">
     <a class="shrink-0" href="https://hackharvard.io">
-      <img class="w-8 h-8" src="/favicon.png" alt="" />
+      <img class="w-8 h-8" src="/favicon.svg" alt="" />
     </a>
     <a class="text-lg uppercase rounded-md bg-blue-200 py-1 px-2 font-bold" href="/"> Portal </a>
   </div>
