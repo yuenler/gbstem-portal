975ea0db30371ee4d2c2358dc486e92467105e79
dseum22 <dseum22@gmail.com>
2023-07-10 12:29:57 +0400

    To ssr

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-pages'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: true,
-      strict: true
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
     })
   },
   preprocess: vitePreprocess()
