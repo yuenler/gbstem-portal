f66f8969f2a9c602c36559ad0c6580d60605e453
dseum22 <dseum22@gmail.com>
2023-08-02 00:42:48 +0900

    Alert sizing fix

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -44,67 +44,66 @@
 
 <svelte:document on:keydown={visible ? handleEscape : undefined} />
 {#if visible}
-  <button
-    type="button"
-    class="h-min-content fixed bottom-3 left-1/2 -translate-x-1/2 z-50 max-w-xl w-full mx-3"
-    on:click={close}
-    transition:fade
-  >
-    <div
-      class={clsx(
-        'flex w-full items-center gap-2 rounded-md p-3 shadow',
-        $alert.type === 'success' && 'bg-green-200',
-        $alert.type === 'info' && 'bg-gray-200',
-        $alert.type === 'error' && 'bg-red-200',
-      )}
-    >
-      {#if $alert.type === 'success'}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="h-6 w-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      {:else if $alert.type === 'info'}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="h-6 w-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-          />
-        </svg>
-      {:else if $alert.type === 'error'}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="h-6 w-6 shrink-0"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-          />
-        </svg>
-      {/if}
-      <div class="grow text-left">{$alert.message}</div>
-    </div>
-  </button>
+  <div class="fixed left-1/2 -translate-x-1/2 w-full max-w-xl bottom-3 px-3">
+    <button class="w-full" type="button" on:click={close} transition:fade>
+      <div
+        class={clsx(
+          'flex w-full items-center gap-2 rounded-md p-3 shadow',
+          $alert.type === 'success' && 'bg-green-200',
+          $alert.type === 'info' && 'bg-gray-200',
+          $alert.type === 'error' && 'bg-red-200',
+        )}
+      >
+        <div class="shrink-0">
+          {#if $alert.type === 'success'}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          {:else if $alert.type === 'info'}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+              />
+            </svg>
+          {:else if $alert.type === 'error'}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6 shrink-0"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+          {/if}
+        </div>
+        <p class="grow text-left">{$alert.message}</p>
+      </div>
+    </button>
+  </div>
 {/if}
