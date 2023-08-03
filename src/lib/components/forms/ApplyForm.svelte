d88ec290f5e62290d59bf0cc49f832fdb934ad3e
dseum22 <dseum22@gmail.com>
2023-08-03 17:44:01 +0900

    Improving application submitted indication and cleaning up email

--- src/lib/components/forms/ApplyForm.svelte
+++ src/lib/components/forms/ApplyForm.svelte
@@ -34,340 +34,7 @@
   import { db, storage, user, type ApplicationData } from '$lib/client/firebase'
   import { cloneDeep, isEqual } from 'lodash-es'
   import Field from '$lib/components/Field.svelte'
-
-  let applicationConfirmationEmail = `
-  <!DOCTYPE HTML
-  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
-  xmlns:o="urn:schemas-microsoft-com:office:office">
-
-<head>
-  <!--[if gte mso 9]>
-<xml>
-  <o:OfficeDocumentSettings>
-    <o:AllowPNG/>
-    <o:PixelsPerInch>96</o:PixelsPerInch>
-  </o:OfficeDocumentSettings>
-</xml>
-<![endif]-->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="x-apple-disable-message-reformatting">
-  <!--[if !mso]><!-->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!--<![endif]-->
-  <title></title>
-
-  <style type="text/css">
-    @media only screen and (min-width: 520px) {
-      .u-row {
-        width: 500px !important;
-      }
-
-      .u-row .u-col {
-        vertical-align: top;
-      }
-
-      .u-row .u-col-100 {
-        width: 500px !important;
-      }
-    }
-
-    @media (max-width: 520px) {
-      .u-row-container {
-        max-width: 100% !important;
-        padding-left: 0px !important;
-        padding-right: 0px !important;
-      }
-
-      .u-row .u-col {
-        min-width: 320px !important;
-        max-width: 100% !important;
-        display: block !important;
-      }
-
-      .u-row {
-        width: 100% !important;
-      }
-
-      .u-col {
-        width: 100% !important;
-      }
-
-      .u-col>div {
-        margin: 0 auto;
-      }
-    }
-
-    body {
-      margin: 0;
-      padding: 0;
-    }
-
-    table,
-    tr,
-    td {
-      vertical-align: top;
-      border-collapse: collapse;
-    }
-
-    p {
-      margin: 0;
-    }
-
-    .ie-container table,
-    .mso-container table {
-      table-layout: fixed;
-    }
-
-    * {
-      line-height: inherit;
-    }
-
-    a[x-apple-data-detectors='true'] {
-      color: inherit !important;
-      text-decoration: none !important;
-    }
-
-    table,
-    td {
-      color: #000000;
-    }
-
-    #u_body a {
-      color: #0000ee;
-      text-decoration: underline;
-    }
-  </style>
-
-
-
-  <!--[if !mso]><!-->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Cabin:400,700" rel="stylesheet" type="text/css">
-  <!--<![endif]-->
-
-</head>
-
-<body class="clean-body u_body"
-  style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #e7e7e7;color: #000000">
-  <!--[if IE]><div class="ie-container"><![endif]-->
-  <!--[if mso]><div class="mso-container"><![endif]-->
-  <table id="u_body"
-    style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #e7e7e7;width:100%"
-    cellpadding="0" cellspacing="0">
-    <tbody>
-      <tr style="vertical-align: top">
-        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
-          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #e7e7e7;"><![endif]-->
-
-
-
-          <div class="u-row-container" style="padding: 0px;background-color: #ffffff">
-            <div class="u-row"
-              style="margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
-              <div
-                style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
-                <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: #ffffff;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:500px;"><tr style="background-color: transparent;"><![endif]-->
-
-                <!--[if (mso)|(IE)]><td align="center" width="500" style="width: 500px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;" valign="top"><![endif]-->
-                <div class="u-col u-col-100"
-                  style="max-width: 320px;min-width: 500px;display: table-cell;vertical-align: top;">
-                  <div style="height: 100%;width: 100% !important;">
-                    <!--[if (!mso)&(!IE)]><!-->
-                    <div
-                      style="box-sizing: border-box; height: 100%; padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
-                      <!--<![endif]-->
-
-                      <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0"
-                        cellspacing="0" width="100%" border="0">
-                        <tbody>
-                          <tr>
-                            <td
-                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;"
-                              align="left">
-
-                              <table width="100%" cellpadding="0" cellspacing="0" border="0">
-                                <tr>
-                                  <td style="padding-right: 0px;padding-left: 0px;" align="center">
-
-                                    <img align="center" border="0"
-                                      src="https://assets.unlayer.com/projects/175801/1691011225210-HHlogo2023.png"
-                                      alt="" title=""
-                                      style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 19%;max-width: 91.2px;"
-                                      width="91.2" />
-
-                                  </td>
-                                </tr>
-                              </table>
-
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-
-                      <!--[if (!mso)&(!IE)]><!-->
-                    </div>
-                    <!--<![endif]-->
-                  </div>
-                </div>
-                <!--[if (mso)|(IE)]></td><![endif]-->
-                <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
-              </div>
-            </div>
-          </div>
-
-
-
-
-
-          <div class="u-row-container" style="padding: 0px;background-color: #ffffff">
-            <div class="u-row"
-              style="margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: #e63d3f;">
-              <div
-                style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
-                <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: #ffffff;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:500px;"><tr style="background-color: #e63d3f;"><![endif]-->
-
-                <!--[if (mso)|(IE)]><td align="center" width="500" style="background-color: #e63d3f;width: 500px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                <div class="u-col u-col-100"
-                  style="max-width: 320px;min-width: 500px;display: table-cell;vertical-align: top;">
-                  <div
-                    style="background-color: #e63d3f;height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
-                    <!--[if (!mso)&(!IE)]><!-->
-                    <div
-                      style="box-sizing: border-box; height: 100%; padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
-                      <!--<![endif]-->
-
-                      <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0"
-                        cellspacing="0" width="100%" border="0">
-                        <tbody>
-                          <tr>
-                            <td
-                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;"
-                              align="left">
-
-                              <h1
-                                style="margin: 0px; color: #ffffff; line-height: 140%; text-align: center; word-wrap: break-word; font-family: 'Montserrat',sans-serif; font-size: 33px; font-weight: 700;">
-                                HACK TO THE FUTURE!</h1>
-
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-
-                      <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0"
-                        cellspacing="0" width="100%" border="0">
-                        <tbody>
-                          <tr>
-                            <td
-                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;"
-                              align="left">
-
-                              <div
-                                style="font-family: 'Cabin',sans-serif; font-size: 25px; color: #b63030; line-height: 140%; text-align: center; word-wrap: break-word;">
-                                <p style="line-height: 140%;"><span
-                                    style="color: #ffffff; line-height: 35px;"><strong><span
-                                        style="line-height: 35px;">October 20-22, 2023</span></strong>
-                                  </span>
-                                </p>
-                              </div>
-
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-
-                      <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0"
-                        cellspacing="0" width="100%" border="0">
-                        <tbody>
-                          <tr>
-                            <td
-                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;"
-                              align="left">
-
-                              <div
-                                style="font-family: 'Montserrat',sans-serif; font-size: 14px; line-height: 140%; text-align: center; word-wrap: break-word;">
-                                <p style="line-height: 140%;"><span style="color: #ecf0f1; line-height: 19.6px;">We're
-                                    so glad you took the time to apply to HackHarvard 2023! Decisions will be released
-                                    at the end of September, and we'll email you once they are available. You can also
-                                    check the status of your application on our portal's <a target="_blank"
-                                      href="https://portal.hackharvard.io/dashboard"
-                                      rel="noopener">dashboard</a>.</span></p>
-                                <p style="line-height: 140%;">&nbsp;</p>
-                                <p style="line-height: 140%;"><span style="color: #ecf0f1; line-height: 19.6px;">In the
-                                    meantime, follow us on <a target="_blank"
-                                      href="https://instagram.com/hackharvardcollege" rel="noopener">Instagram</a> so
-                                    you can stay up to date on all the exciting things we've planned for HackHarvard
-                                    2023!</span></p>
-                                <p style="line-height: 140%;">&nbsp;</p>
-                                <p style="line-height: 140%;"><span style="color: #ecf0f1; line-height: 19.6px;">If you
-                                    have any questions or concerns, visit our <a target="_blank"
-                                      href="https://hackharvard.io/" rel="noopener">FAQs</a> or you can reach us at <a
-                                      target="_blank" href="mailto:team@hackharvard.io"
-                                      rel="noopener">team@hackharvard.io</a>.</span></p>
-                                <p style="line-height: 140%;">&nbsp;</p>
-                                <p style="line-height: 140%;"><span style="color: #ecf0f1; line-height: 19.6px;">With
-                                    love 🤍,</span></p>
-                                <p style="line-height: 140%;"><span style="color: #ecf0f1; line-height: 19.6px;">The
-                                    HackHarvard Team</span></p>
-                              </div>
-
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-
-                      <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0"
-                        cellspacing="0" width="100%" border="0">
-                        <tbody>
-                          <tr>
-                            <td
-                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;"
-                              align="left">
-
-                              <table width="100%" cellpadding="0" cellspacing="0" border="0">
-                                <tr>
-                                  <td style="padding-right: 0px; padding-left: 0px;" align="center">
-                                    <a href="https://www.youtube.com/watch?v=Q-2PzbXm5-Q&t=1s" target="_blank">
-                                      <img align="center" border="0"
-                                        src="https://assets.unlayer.com/projects/175801/1691013406561-video_youtube_Q-2PzbXm5-Q.jpg"
-                                        alt="Video" title="Video"
-                                        style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;width: 100%;max-width: 480px;"
-                                        width="480" class="fullwidth" />
-                                    </a>
-                                  </td>
-                                </tr>
-                              </table>
-
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-
-                      <!--[if (!mso)&(!IE)]><!-->
-                    </div>
-                    <!--<![endif]-->
-                  </div>
-                </div>
-                <!--[if (mso)|(IE)]></td><![endif]-->
-                <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
-              </div>
-            </div>
-          </div>
-
-
-
-          <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  <!--[if mso]></div><![endif]-->
-  <!--[if IE]></div><![endif]-->
-</body>
-
-</html>
-  `
+  import Button from '../Button.svelte'
 
   let disabled = true
   let showValidation = false
@@ -582,6 +249,10 @@
                       top: 0,
                       behavior: 'smooth',
                     })
+                    alert.trigger(
+                      'success',
+                      'Your application has been submitted!',
+                    )
                     handleEmail()
                   },
                 )
@@ -604,9 +275,8 @@
   function handleEmail() {
     return addDoc(collection(db, 'mail'), {
       to: [values.personal.email],
-      message: {
-        subject: 'HackHarvard Application Submitted!',
-        html: applicationConfirmationEmail,
+      template: {
+        name: 'application',
       },
     })
   }
@@ -625,6 +295,13 @@
   on:submit={handleSubmit}
 >
   <fieldset class="space-y-14" {disabled}>
+    {#if values.meta.submitted}
+      <div
+        class="rounded-md bg-green-100 px-4 py-2 text-center text-green-900 shadow-sm"
+      >
+        Application submitted and in review!
+      </div>
+    {/if}
     <div class="grid gap-4">
       <span class="font-bold text-2xl">Personal</span>
       <Card class="space-y-3">
@@ -990,26 +667,9 @@
       />
     </div>
     <div class={clsx('grid gap-3', !values.meta.submitted && 'grid-cols-2')}>
-      {#if values.meta.submitted}
-        <div
-          class="rounded-md bg-green-100 px-4 py-2 text-center text-green-900 shadow-sm"
-        >
-          Application submitted and in review!
-        </div>
-      {:else}
-        <button
-          type="button"
-          on:click={() => handleSave(true)}
-          class="rounded-md bg-gray-100 px-4 py-2 shadow-sm transition-colors duration-300 hover:bg-gray-200 disabled:bg-gray-200 disabled:text-gray-500"
-        >
-          Save draft
-        </button>
-        <button
-          type="submit"
-          class="rounded-md bg-blue-100 px-4 py-2 text-blue-900 shadow-sm transition-colors duration-300 hover:bg-blue-200 disabled:bg-blue-200 disabled:text-blue-500"
-        >
-          Submit
-        </button>
+      {#if !values.meta.submitted}
+        <Button on:click={() => handleSave(true)}>Save draft</Button>
+        <Button color="blue" type="submit">Submit</Button>
       {/if}
     </div>
   </fieldset>
