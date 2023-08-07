9be26fe6ec73c74bc0b0e42015d2b410ba9e2488
dseum22 <dseum22@gmail.com>
2023-08-07 11:45:43 +0900

    Updating favicon

--- src/lib/components/Brand.svelte
+++ src/lib/components/Brand.svelte
@@ -3,7 +3,7 @@
     class="inline-flex items-center gap-2 rounded-md border border-gray-200 p-2 shadow"
   >
     <a class="shrink-0" href="https://hackharvard.io" target="_blank">
-      <img class="h-8 w-8" src="/favicon.svg" alt="" />
+      <img class="h-8 w-8" src="/favicon.png" alt="" />
     </a>
     <a
       class="rounded-md bg-blue-200 px-2 py-1 text-lg font-bold uppercase"
