e1d0e0c319149cbd0a1d2e2ce8c61328d7212b03
Yuen Ler Chow <lerchow@gmail.com>
2023-10-27 01:07:27 -0400

    certificate

--- src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
+++ src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { db, user } from '$lib/client/firebase'
+  import { db, user, storage } from '$lib/client/firebase'
   import Card from '$lib/components/Card.svelte'
   import Dialog from '$lib/components/Dialog.svelte'
   import DialogActions from '$lib/components/DialogActions.svelte'
@@ -10,6 +10,87 @@
   import { getDoc, doc } from 'firebase/firestore'
   import { fade } from 'svelte/transition'
   import Button from '$lib/components/Button.svelte'
+  import { onMount } from 'svelte'
+  import { fill } from 'lodash-es'
+
+  let pdfUrl =
+    'https://raw.githubusercontent.com/hackharvard/portal/7c95d9c559956a4a4f414583a05a9391f2d25d76/certificates.pdf'
+  let pageNumber = 0
+
+  let text = ''
+  let cantFind = false
+  onMount(() => {
+    return user.subscribe(async (user) => {
+      if (!user) return
+      const data = await fetch('src/data.csv')
+      text = await data.text()
+      const rows = text.split('\n')
+      const headers = rows[0].split(',')
+      const fullNameIndex = headers.indexOf('team member')
+      const fullName = user.profile.firstName + ' ' + user.profile.lastName
+      const fullNameRow = rows.find(
+        (row) => row.split(',')[fullNameIndex] === fullName,
+      )
+      if (fullNameRow) {
+        const fullNameRowNumber = rows.indexOf(fullNameRow)
+        pageNumber = fullNameRowNumber
+      } else {
+        cantFind = true
+      }
+    })
+  })
+
+  let fullName = ''
+
+  // when full name changes
+  $: {
+    if (fullName) {
+      const rows = text.split('\n')
+      const headers = rows[0].split(',')
+      const fullNameIndex = headers.indexOf('team member')
+      const fullNameRow = rows.find(
+        (row) => row.split(',')[fullNameIndex] === fullName,
+      )
+      if (fullNameRow) {
+        const fullNameRowNumber = rows.indexOf(fullNameRow)
+        pageNumber = fullNameRowNumber
+      }
+    }
+  }
+
+  async function downloadCertificate() {
+    if (pdfUrl) {
+      const pdfjs = await import('pdfjs-dist')
+      pdfjs.GlobalWorkerOptions.workerSrc = 'src/pdf.worker.js'
+
+      const pdf = await pdfjs.getDocument(pdfUrl).promise
+      const page = await pdf.getPage(pageNumber)
+
+      const scale = 8.0 // Increase the scale for even higher resolution
+      const viewport = page.getViewport({ scale })
+
+      const canvas = document.createElement('canvas')
+      const context = canvas.getContext('2d')
+      canvas.height = viewport.height
+      canvas.width = viewport.width
+
+      const renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+      }
+
+      await page.render(renderContext).promise
+
+      // Get the image data from the canvas as a high-resolution PNG
+      const imageData = canvas.toDataURL('image/png', 1.0) // 1.0 quality for high resolution
+
+      // Create a download link for the PNG
+      const a = document.createElement('a')
+      a.href = imageData
+      a.download = `certificate.png`
+      a.click()
+    }
+  }
 
   type ApplicationStatus = Data.Decision | 'submitted' | null
   type DashboardData = {
@@ -70,9 +151,10 @@
 <Dialog bind:this={dialogEl} size="full">
   <svelte:fragment slot="title">Hacker Guide</svelte:fragment>
   <div slot="description" class="space-y-4">
-    <iframe 
-    class= "w-full h-[calc(100vh-300px)]"
-    src="https://docs.google.com/document/d/e/2PACX-1vSG9CCzX3-K99gWP7SjdzaxQ9APRbbhXi2pwlWiFig2q9sbFh3TDjkFY7BQZj3dZXRgf-Ed6VKIqBRc/pub?embedded=true"></iframe>
+    <iframe
+      class="h-[calc(100vh-300px)] w-full"
+      src="https://docs.google.com/document/d/e/2PACX-1vSG9CCzX3-K99gWP7SjdzaxQ9APRbbhXi2pwlWiFig2q9sbFh3TDjkFY7BQZj3dZXRgf-Ed6VKIqBRc/pub?embedded=true"
+    ></iframe>
     <DialogActions>
       <Button on:click={dialogEl.cancel}>Close</Button>
     </DialogActions>
@@ -127,10 +209,38 @@
             <Link href="/apply">View application</Link>
             {#if data.application.status === 'accepted'}
               <div>
-              <button on:click={dialogEl.open} class="inline-block border-b border-black text-black transition-colors duration-300 hover:border-gray-600 hover:text-gray-600">Hacker Guide</button>
-            </div>
+                <button
+                  on:click={dialogEl.open}
+                  class="inline-block border-b border-black text-black transition-colors duration-300 hover:border-gray-600 hover:text-gray-600"
+                  >Hacker Guide</button
+                >
+              </div>
             {/if}
-            
+
+            {#if data.application.status === 'accepted' && cantFind}
+              <p>
+                To get your certificate, please enter your name just as you
+                filled it out on the presubmission form below. If no result
+                appears below, please contact us at <a
+                  href="mailto:team@hackharvard.io">team@hackharvard.io</a
+                >.
+              </p>
+              <input
+                type="text"
+                class="w-full rounded-md border border-gray-300 px-4 py-2"
+                bind:value={fullName}
+              />
+            {/if}
+
+            {#if data.application.status === 'accepted' && pageNumber > 0}
+              <div>
+                <button
+                  on:click={() => downloadCertificate()}
+                  class="inline-block border-b border-black text-black transition-colors duration-300 hover:border-gray-600 hover:text-gray-600"
+                  >Download certificate of completion</button
+                >
+              </div>
+            {/if}
           </div>
         </Card>
         {#if data.application.status === 'accepted'}
