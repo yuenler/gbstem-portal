298cfd22e5b952fcb42e914b5af7dae619dceabd
Yuen Ler Chow <lerchow@gmail.com>
2024-01-22 02:31:39 -0500

    remove firstname in reset password email

--- src/lib/components/forms/ResetPasswordForm.svelte
+++ src/lib/components/forms/ResetPasswordForm.svelte
@@ -17,33 +17,30 @@
     if (e.detail.error === null) {
       showValidation = false
       disabled = true
-      if ($user) {
-        fetch('/api/action', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            type: 'resetPassword',
-            email: values.email,
-            firstName: $user.profile.firstName,
-          }),
-        }).then(async (res) => {
-          if (res.ok) {
-            alert.trigger(
-              'info',
-              'Password reset email was sent. Please check your inbox.',
-            )
-          } else {
-            const { message } = await res.json()
-            alert.trigger('error', message)
-          }
-          values = {
-            email: '',
-          }
-          disabled = false
-        })
-      }
+      fetch('/api/action', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          type: 'resetPassword',
+          email: values.email,
+        }),
+      }).then(async (res) => {
+        if (res.ok) {
+          alert.trigger(
+            'info',
+            'Password reset email was sent. Please check your inbox.',
+          )
+        } else {
+          const { message } = await res.json()
+          alert.trigger('error', message)
+        }
+        values = {
+          email: '',
+        }
+        disabled = false
+      })
     } else {
       showValidation = true
       alert.trigger('error', e.detail.error)
--- src/routes/api/action/+server.ts
+++ src/routes/api/action/+server.ts
@@ -68,11 +68,11 @@ export const POST: RequestHandler = async ({ request, locals }) => {
           break
         }
         case 'resetPassword': {
+          console.log(body.email)
           const link = await adminAuth.generatePasswordResetLink(body.email)
           to = body.email
           data = {
             subject: 'Reset Password for gbSTEM Account',
-            name: firstName,
             action: {
               link,
               name: 'Reset Password',
@@ -111,7 +111,9 @@ export const POST: RequestHandler = async ({ request, locals }) => {
         MessageStream: 'outbound'
       }
 
+
       try {
+        console.log('sending email')
         const client = new postmark.ServerClient(POSTMARK_API_TOKEN);
         client.sendEmail(emailData);
         return new Response()
