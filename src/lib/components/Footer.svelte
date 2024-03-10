14d3b46fefdc84804cc743bc62401726df9542e1
Yuen Ler Chow <lerchow@gmail.com>
2024-03-10 17:03:56 -0400

    Add bug report link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,9 @@
 <footer class="flex items-center justify-center py-6">
-  <span>&copy; {new Date().getFullYear()} gbSTEM</span>
+  <span
+    >&copy; {new Date().getFullYear()} gbSTEM ❤️
+    <a
+      href="mailto:contact@gbstem.org?subject=Bug Report"
+      style="text-decoration: underline">Report a Bug</a
+    >
+  </span>
 </footer>
