31f68d4a1b09da3d8b93dc177666618d62c20dfa
Kendree Chen <kendree.chen@gmail.com>
2024-07-15 10:34:52 -0400

    move classdetails to types

---
+++ src/lib/components/types/ClassDetails.ts
@@ -0,0 +1,20 @@
+export type ClassDetails = {
+    classDay1: string
+    classTime1: string
+    classDay2: string
+    classTime2: string
+    meetingLink: string
+    gradeRecommendation: string
+    course: string
+    submitting: boolean
+    meetingTimes: Date[]
+    datesHeld: Date[]
+    feedbackCompleted: boolean[]
+    classStatuses: string[]
+    instructorFirstName: string
+    instructorLastName: string
+    instructorEmail: string
+    otherInstructorEmails: string
+    classCap: number
+    online: boolean
+  }
