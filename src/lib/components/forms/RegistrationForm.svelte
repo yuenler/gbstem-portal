65b297c7e71806f97c129bc1b2d73c2249b0b9d0
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-08-16 22:53:38 -0400

    reword

--- src/lib/components/forms/RegistrationForm.svelte
+++ src/lib/components/forms/RegistrationForm.svelte
@@ -792,7 +792,7 @@
       <Input
         type="checkbox"
         bind:value={values.program.inPerson}
-        label="For our in-person offering in the fall, gbSTEM is holding a new Lego Robotics competition program for students grade 5 and up. The program will meet weekly in-person at the Cambridge Public Library on Saturdays 1:00-3:00pm; parents are welcome to help coach the robotics team. There are 10 slots available this year and will be more in the future. You may apply for this program on top of two courses, but if you are selected you will only be able to enroll in one additional course. Would you like to apply for the robotics program?"
+        label="For our in-person offering in the fall, gbSTEM is holding a new Lego Robotics competition program for students grade 5 and up. The program will meet weekly in-person at the Cambridge Public Library on Saturdays 1:00-3:00pm; parents are encouraged to help coach the robotics team. There are 10 slots available this year and will be more in the future. You may apply for this program on top of two courses, but if you are selected you will only be able to enroll in one additional course. Would you like to apply for the robotics program?"
       />
       {#if values.program.inPerson}
         <Input
