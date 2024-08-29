f3be26a9e672016cc5cac5c0b5ac56d6fc8fc65c
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-08-29 03:09:50 -0400

    fix import url

--- src/lib/components/forms/ClassDetailsForm.svelte
+++ src/lib/components/forms/ClassDetailsForm.svelte
@@ -18,7 +18,7 @@
 
   import Dialog from '../Dialog.svelte'
   import Card from '../Card.svelte'
-  import CLIENT_ID from '$env/static/public'
+  import CLIENT_ID from '$env/dynamic/private'
 
  export let classDetailsDialogEl: Dialog | undefined
  export let dialog = false
