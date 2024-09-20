c44cde23592278cc9655b4e16eb7e2c3bb463edd
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-09-19 23:33:26 -0400

    fix substitute thing

--- src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
+++ src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
@@ -213,9 +213,9 @@
       
       {#if data.application.status === 'accepted'}
         <ClassDetailsForm semesterDates = {semesterDates} classDetailsDialogEl={undefined} dialog={false}/>
-          <SubClasses />
+          <SubClasses subInstructor={false} />
       {:else if data.application.status === 'substitute'}
-          <SubClasses />
+          <SubClasses subInstructor={true}/>
       {:else if isStudent && new Date() > new Date(semesterDates.classesStart)}
         <StudentSchedule />
       {/if}
