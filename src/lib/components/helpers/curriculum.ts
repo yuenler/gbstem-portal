91c5c498e7b81b684c3b7c125be6e693ba458809
Kendree <94144671+kchenturtles@users.noreply.github.com>
2025-09-20 22:30:59 -0400

    fix curriculum

--- src/lib/components/helpers/curriculum.ts
+++ src/lib/components/helpers/curriculum.ts
@@ -1,80 +1,156 @@
 import type Curriculum from "../types/Curriculum";
 
 export const tracks: string[] = ["Math", "Science", "Computer Science", "Engineering"]
+const baseUrl = "https://curriculum.gbstem.org/#/"
 export const curriculums: Curriculum[] = [
-    // {
-    //     class: "Math I",
-    //     url: "/curriculum/grade1",
-    //     track: "Math"
-    // },
+    {
+        class: "Mathematics 1a",
+        url: baseUrl + "/math/math1A",
+        track: "math"
+    },
+    {
+        class: "Mathematics 1b",
+        url: baseUrl + "/math/math1B",
+        track: "math"
+    },
     {
         class: "Mathematics 2a",
-        url: "https://docs.google.com/document/d/1UKetFu3NuVbf3-Qi7HMI3nzuRR7qJAUYdAi_ZFIND6o",
-        track: "Math"
+        url: baseUrl + "/math/math2A",
+        track: "math"
+    },
+    {
+        class: "Mathematics 2b",
+        url: baseUrl + "/math/math2B",
+        track: "math"
     },
     {
         class: "Mathematics 3a",
-        url: "https://docs.google.com/document/d/1SRex4R5A3J1By2k3EqpSUa_qN3F8DIvkI9CDddt2C_g",
-        track: "Math"
+        url: baseUrl + "/math/math3A",
+        track: "math"
+    },
+    {
+        class: "Mathematics 3b",
+        url: baseUrl + "/math/math3B",
+        track: "math"
     },
     {
         class: "Mathematics 4a",
-        url: "https://docs.google.com/document/d/1qkYlxOeKNjv-D_bEUfvKzFcMWZBRcfB09N-Vcri_TD4",
+        url: baseUrl + "/math/math4A",
+        track: "Math"
+    },
+    {
+        class: "Mathematics 4b",
+        url: baseUrl + "/math/math4B",
         track: "Math"
     },
     {
         class: "Mathematics 5a",
-        url: "https://docs.google.com/document/d/1cxkbg8yGP1h7Rub8Sc-xC67iipxG_GWPaDAxhTZdm5I",
+        url: baseUrl + "/math/math5A",
         track: "Math"
     },
     {
-        class: "Scratch",
-        url: "https://docs.google.com/document/d/1uDu9cKSnt9jRWusmSj17iVAx6b16-L0gZjWhrhqZr_k",
+        class: "Mathematics 5b",
+        url: baseUrl + "/math/math5B",
+        track: "Math"
+    },
+    {
+        class: "Scratch 1a",
+        url: baseUrl + "/cs/scratch1A",
         track: "Computer Science"
     },
     {
-        class: "Scratch II",
-        url: "https://docs.google.com/document/d/1uDu9cKSnt9jRWusmSj17iVAx6b16-L0gZjWhrhqZr_k",
+        class: "Scratch 1b",
+        url: baseUrl + "/cs/scratch1B",
         track: "Computer Science"
     },
     {
-        class: "Python I",
-        url: "https://docs.google.com/document/d/1-EYnFZEOT7hG1FMSrGaRwKqg45Ymq1T_HhZWxRYw6KQ",
+        class: "Scratch 2a",
+        url: baseUrl + "/cs/scratch2A",
         track: "Computer Science"
     },
     {
-        class: "Python II",
-        url: "https://docs.google.com/document/d/1aFjwWeQR2arqT_CXmGOMkBwHjRIcZCkTL0TJFBPFY4k",
+        class: "Scratch 2b",
+        url: baseUrl + "/cs/scratch2B",
         track: "Computer Science"
     },
     {
-        class: "Web Development",
-        url: "https://docs.google.com/document/d/1Mpw5i_T827dKXpDxZdbLmO263JSpigUZQbDbLDKuPYM",
+        class: "Python 1a",
+        url: baseUrl + "/cs/python1A",
         track: "Computer Science"
     },
     {
-        class: "Engineering I",
-        url: "/curriculum/grade1",
+        class: "Python 1b",
+        url: baseUrl + "/cs/python1B",
+        track: "Computer Science"
+    },
+    {
+        class: "Python 2a",
+        url: baseUrl + "/cs/python2A",
+        track: "Computer Science"
+    },
+    {
+        class: "Python 2b",
+        url: baseUrl + "/cs/python2B",
+        track: "Computer Science"
+    },
+    {
+        class: "Web Development A",
+        url: baseUrl + "/cs/webdevA",
+        track: "Computer Science"
+    },
+    {
+        class: "Web Development B",
+        url: baseUrl + "/cs/webdevB",
+        track: "Computer Science"
+    },
+    {
+        class: "Engineering 1a",
+        url: baseUrl + "/engineering/engineering1A",
         track: "Engineering"
     },
     {
-        class: "Engineering II",
-        url: "/curriculum/grade1",
+        class: "Engineering 1b",
+        url: baseUrl + "/engineering/engineering1B",
         track: "Engineering"
     },
     {
-        class: "Engineering III",
-        url: "/curriculum/grade1",
+        class: "Engineering 2a",
+        url: baseUrl + "/engineering/engineering2A",
         track: "Engineering"
     },
     {
-        class: "Environmental Science",
-        url: "https://docs.google.com/document/d/1G_uu26Vfh59wUOwwPlyGH4P9SxeofibaPqB7szl-h44",
+        class: "Engineering 2b",
+        url: baseUrl + "/engineering/engineering2B",
+        track: "Engineering"
+    },
+    {
+        class: "Engineering 3a",
+        url: baseUrl + "/engineering/engineering3A",
+        track: "Engineering"
+    },
+    {
+        class: "Engineering 3b",
+        url: baseUrl + "/engineering/engineering3B",
+        track: "Engineering"
+    },
+    {
+        class: "Environmental Science A",
+        url: baseUrl + "/science/environmentalA",
         track: "Science"
     },
     {
-        class: "Physics",
-        url: "https://docs.google.com/document/d/1V-DaLpZxVTKHqfAhmZnZ1WkZcp2Haw5z-1xg59aSRnA",
+        class: "Environmental Science B",
+        url: baseUrl + "/science/environmentalB",
         track: "Science"
     },
+    {
+        class: "Physics A",
+        url: baseUrl + "/science/physicsA",
+        track: "Science"
+    },
+    {
+        class: "Physics B",
+        url: baseUrl + "/science/physicsB",
+        track: "Science"
+    }
 ]
